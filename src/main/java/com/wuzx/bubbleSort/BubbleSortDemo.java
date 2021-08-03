e77070dba3d4e8756aa3989a7f252a32e8fae606
wuzhixuan <zhixuan.wu@oigcn.com>
2021-08-03 18:15:21 +0800

    冒泡

---
+++ src/main/java/com/wuzx/bubbleSort/BubbleSortDemo.java
@@ -0,0 +1,33 @@
+package com.wuzx.bubbleSort;
+
+/**
+ * @author wuzhixuan
+ * @version 1.0.0
+ * @ClassName BubbleSortDemo.java
+ * @Description 冒泡排序
+ * @createTime 2021年08月03日 17:54:00
+ */
+public class BubbleSortDemo {
+
+    public void bubbleSort(int[] arr) {
+        if (null == arr) {
+            return;
+        }
+
+
+
+        // 0-1 交换
+        // 1-2 交换
+        for (int i = 0; i < arr.length; i++) {
+
+            for (int j = 1;;) {
+
+            }
+
+
+        }
+
+
+
+    }
+}
